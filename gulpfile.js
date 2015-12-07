ce39a5816af51029d280f56d071010dfda387566
Jamen Marz <jamenmarz@gmail.com>
2015-12-06 22:27:40 -0700

    Update gulpfile to minify

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,32 @@ const gulp = require('gulp'),
       webpack = require('gulp-webpack'),
       stylus = require('gulp-stylus'),
       rename = require('gulp-rename'),
+      uglify = require('gulp-uglify'),
+      minCSS = require('gulp-minify-css'),
+      minHTML = require('gulp-minify-html'),
       swig = require('gulp-swig'),
       fake = (a) => { process.argv = [ '', '', ...a]; },
       dist = 'dist/';
 
+let config = {
+  output:{ filename:'app.js' },
+  cache:false,
+  module:{ loaders:[
+    {
+      loader:'babel', test:/\.jsx?$/,
+      exclude:/(node_modules|dist)/,
+      query:{ presets:[ 'es2015' ], plugins:[ 'transform-runtime' ] }
+    }
+  ]}
+};
+
 // Webpack
 gulp.task('build:javascript', () =>
-  gulp.src('scripts/*.js')
-    .pipe(webpack({
-      output:{ filename:'app.js' },
-      module:{ loaders:[
-        {
-          loader:'babel', test:/\.jsx?$/,
-          exclude:/(node_modules|bower_components|dist)/,
-          query:{ presets:[ 'es2015' ], plugins:[ 'transform-runtime' ] }
-        }
-      ]}
-    }))
+  gulp.src(['scripts/*.js'])
+    .pipe(webpack(config))
+    .pipe(gulp.dest(dist))
+    .pipe(rename('app.min.js'))
+    .pipe(uglify({ mangle: false }))
     .pipe(gulp.dest(dist))
 );
 
@@ -41,19 +50,54 @@ gulp.task('build:swig', () =>
     .pipe(gulp.dest(dist))
 );
 
-// Alias for all
-gulp.task('build', [ 'build:swig', 'build:stylus', 'build:javascript' ]);
+// Minification
+gulp.task('minify:javascript', [ 'build:javascript' ], () =>
+  gulp.src('dist/app.js')
+    .pipe(rename('app.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'))
+);
 
-// Set default to build all
-gulp.task('default', [ 'build' ]);
+gulp.task('minify:css', [ 'build:stylus' ], () =>
+  gulp.src('dist/app.css')
+    .pipe(rename('app.min.css'))
+    .pipe(minCSS())
+    .pipe(gulp.dest('dist'))
+);
+
+gulp.task('minify:html', [ 'build:swig' ], () =>
+  gulp.src('dist/app.html')
+    .pipe(rename('app.min.html'))
+    .pipe(minHTML())
+    .pipe(gulp.dest('dist'))
+);
+
+// Build
+gulp.task('build', [
+  'build:swig',
+  'build:stylus',
+  'build:javascript'
+]);
+
+
+// Minify
+gulp.task('minify', [
+  'build',
+  'minify:javascript',
+  'minify:css',
+  'minify:html'
+]);
+
+// Alias for build & minify
+gulp.task('default', [ 'minify' ]);
 
 // Watch
-gulp.task('watch', [ 'default' ], function(){
-  gulp.watch('views/**', [ 'build:swig' ]);
-  gulp.watch('styles/**', [ 'build:stylus' ]);
-  gulp.watch('scripts/**', [ 'build:javascript' ]);
+gulp.task('watch', [ 'minify' ], function(){
+  gulp.watch('views/**', [ 'build:swig', 'minify:html' ]);
+  gulp.watch('styles/**', [ 'build:stylus', 'minify:css' ]);
+  gulp.watch('scripts/**', [ 'build:javascript', 'minify:javascript' ]);
 
   // Create fake app-server:
-  fake(['-c', 'app.json', '--port=8080', '--serve.maxAge="0"']);
+  fake([ '-c', 'app.json', '--port=8080', '--serve.maxAge="0"' ]);
   require('app-server');
 });
