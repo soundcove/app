7e18db51c64c426396ab5cdf187b4849b7c3eb28
Dustin Dowell <dustindowell22@gmail.com>
2016-05-17 11:06:41 -0500

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,100 +1,42 @@
-'use strict';
+'use strict'
 
-const gulp = require('gulp'),
-  webpack = require('gulp-webpack'),
-  stylus = require('gulp-stylus'),
-  rename = require('gulp-rename'),
-  uglify = require('gulp-uglify'),
-  minCSS = require('gulp-minify-css'),
-  minHTML = require('gulp-minify-html'),
-  swig = require('gulp-swig'),
-  fake = a => {
-    process.argv = ['', '', ...a];
-  },
-  dist = 'dist/';
+// Node Modules
+var gulp       = require('gulp')
+var browserify = require('browserify')
+var source     = require('vinyl-source-stream')
 
-let config = {
-  output: {filename: 'app.js'},
-  cache: false,
-  module: {loaders: [
-    {
-      loader: 'babel',
-      exclude: /(node_modules|dist)/,
-      query: {presets: ['es2015'], plugins: ['transform-runtime']}
-    }
-  ]}
-};
+// Gulp Plugins
+var sass = require('gulp-sass')
+var jade = require('gulp-jade')
 
-// Webpack
-gulp.task('build:javascript', () =>
-  gulp.src(['scripts/*.js'])
-    .pipe(webpack(config))
-    .pipe(gulp.dest(dist))
-    .pipe(rename('app.min.js'))
-    .pipe(uglify({mangle: false}))
-    .pipe(gulp.dest(dist))
-);
+// Sass
+gulp.task('sass', function () {
+  gulp.src('scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('css'))
+})
 
-// Stylus
-gulp.task('build:stylus', () =>
-  gulp
-    .src('styles/index.styl')
-    .pipe(stylus())
-    .pipe(rename('app.css'))
-    .pipe(gulp.dest(dist))
-);
+// Jade
+gulp.task('jade', function () {
+  gulp.src('*.jade')
+    .pipe(jade({ pretty: true }))
+    .pipe(gulp.dest(''))
+})
 
-// Swig
-gulp.task('build:swig', () =>
-  gulp.src('views/index.html')
-    .pipe(swig({defaults: {cache: false}}))
-    .pipe(rename('app.html'))
-    .pipe(gulp.dest(dist))
-);
-
-// Minification
-gulp.task('minify:javascript', ['build:javascript'], () =>
-  gulp.src('dist/app.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist'))
-);
-
-gulp.task('minify:css', ['build:stylus'], () =>
-  gulp.src('dist/app.css')
-    .pipe(minCSS())
-    .pipe(gulp.dest('dist'))
-);
-
-gulp.task('minify:html', ['build:swig'], () =>
-  gulp.src('dist/app.html')
-    .pipe(minHTML())
-    .pipe(gulp.dest('dist'))
-);
+// Browserify
+gulp.task('browserify', function() {
+  browserify('./index.js').bundle()
+    .pipe(source('index.js'))
+    .pipe(gulp.dest(''))
+})
 
 // Build
-gulp.task('build', [
-  'build:swig',
-  'build:stylus',
-  'build:javascript'
-]);
-
-// Minify
-gulp.task('minify', [
-  'minify:javascript',
-  'minify:css',
-  'minify:html'
-]);
-
-// Alias for build & minify
-gulp.task('default', ['build']);
+gulp.task('build', ['sass', 'jade'])
+// gulp.task('build', ['sass', 'jade', 'browserify'])
 
 // Watch
-gulp.task('watch', ['build'], function() {
-  gulp.watch('views/**', ['build:swig']);
-  gulp.watch('styles/**', ['build:stylus']);
-  gulp.watch('scripts/**', ['build:javascript']);
-
-  // Create fake app-server:
-  fake(['-c', 'fake-app.json']);
-  require('app-server');
-});
+gulp.task('watch', function () {
+  gulp.watch('scss/**/*.scss', ['build'])
+  gulp.watch('index.jade',     ['build'])
+  // gulp.watch('index.js',       ['build'])
+})
