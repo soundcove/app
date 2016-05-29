73f55d7acc9815cd9bbeaf0477c3f8ffcfb2b72d
Dustin Dowell <dustindowell22@gmail.com>
2016-05-28 21:26:46 -0500

    inline css and js

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,32 @@
 'use strict'
 
-// Node Modules
 var gulp       = require('gulp')
 var browserify = require('browserify')
 var source     = require('vinyl-source-stream')
 
-// Gulp Plugins
 var sass = require('gulp-sass')
 var jade = require('gulp-jade')
 
-// Sass
 gulp.task('sass', function () {
   gulp.src('source/index.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(''))
+    .pipe(gulp.dest('tmp/'))
 })
 
-gulp.task('sass:pretty', function () {
-  gulp.src('source/index.scss')
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(gulp.dest(''))
+gulp.task('browserify', function() {
+  browserify('./source/index.js').bundle()
+    .pipe(source('index.js'))
+    .pipe(gulp.dest('./tmp/'))
 })
 
-// Jade
-gulp.task('jade', function () {
+gulp.task('jade', ['sass', 'browserify'], function () {
   gulp.src('source/index.jade')
     .pipe(jade())
     .pipe(gulp.dest(''))
 })
 
-gulp.task('jade:pretty', function () {
-  gulp.src('source/index.jade')
-    .pipe(jade({ pretty: true }))
-    .pipe(gulp.dest(''))
-})
+gulp.task('build', ['jade'])
 
-// Browserify
-gulp.task('browserify', function() {
-  browserify('./source/index.js').bundle()
-    .pipe(source('index.js'))
-    .pipe(gulp.dest('./'))
-})
-
-// Build
-gulp.task('build',        ['sass',        'jade',        'browserify'])
-gulp.task('build:pretty', ['sass:pretty', 'jade:pretty', 'browserify'])
-
-// Watch
 gulp.task('watch', function () {
   gulp.watch('source/**/*', ['build'])
-})
-
-gulp.task('watch:pretty', function () {
-  gulp.watch('source/**/*', ['build:pretty'])
 })
