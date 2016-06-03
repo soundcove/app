b69aa2df00136b1b43346ee1128be382ba93e5b9
Dustin Dowell <dustindowell22@gmail.com>
2016-06-03 16:34:28 -0500

    svg

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,10 @@ var gulp       = require('gulp')
 var browserify = require('browserify')
 var source     = require('vinyl-source-stream')
 
-var sass    = require('gulp-sass')
-var jade    = require('gulp-jade')
-var postcss = require('gulp-postcss')
+var sass       = require('gulp-sass')
+var jade       = require('gulp-jade')
+var postcss    = require('gulp-postcss')
+var svgSymbols = require('gulp-svg-symbols')
 
 var autoprefixer = require('autoprefixer')
 
@@ -20,6 +21,12 @@ gulp.task('css', function () {
 gulp.task('browserify', function() {
   browserify('src/index.js').bundle()
     .pipe(source('index.js'))
+    .pipe(gulp.dest('tmp/'))
+})
+
+gulp.task('svg', function () {
+  gulp.src('src/**/*.svg')
+    .pipe(svgSymbols())
     .pipe(gulp.dest('tmp/'))
 })
 
